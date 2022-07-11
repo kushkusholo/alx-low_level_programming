a4921d60c415a42e996463d99b01fc54b9198f66
kushkusholo <maikiprotus81@gmail.com>
2022-07-11 11:56:54 -0700

    printing alphabete in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - prints the alphabet in lower case
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
